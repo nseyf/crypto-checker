2d58d8ec5dedda6790e5a8d5e1a2c2214d6187d3
Nima Seyf <nimaseyf@gmail.com>
2017-11-13 13:01:43 +0000

    fixed loader component

--- src/components/loader.js
+++ src/components/loader.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 export default function() {
   return (
-    <div>
-Loading!
+    <div style={{
+      left: "0",
+    lineHeight: "200px",
+    marginTop: "-100px",
+    position: "absolute",
+    textAlign: "center",
+    top:"50%",
+    width: "100%"
+      }}>
+      <i style={{color: "white"}} className="fa fa-circle-o-notch fa-spin fa-4x fa-fw"></i>
+      <span className="sr-only">Loading...</span>
     </div>
   )
 }
